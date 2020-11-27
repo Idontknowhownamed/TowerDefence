b8b6dddce69ebe77ba09b6322f56fd63baa35ba4
Artyom <Idontknowhownamed@users.noreply.github.com>
2020-11-27 08:23:35 +0300

    Ne poshlo(

---
+++ Assets/moveCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour
+{
+    public int speed;
+    Vector2 touchstart;
+    public float maxZoom, minZoom, sensivity;
+
+    public float x, y;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            touchstart = GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);        
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 direction = touchstart;
+            GetComponent<Camera>().transform.position = direction;
+        }
+        ZoomCamera(Input.GetAxis("Mouse ScrollWheel"));
+
+    }
+
+    void ZoomCamera(float increment)
+    {
+        GetComponent<Camera>().orthographicSize = Mathf.Clamp(GetComponent<Camera>().orthographicSize - increment * sensivity, minZoom, maxZoom);
+    }
+
+}
